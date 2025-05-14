10423cd897e6e6b004235d6aa08b9fb23963963d
Garrick Aden-Buie <garrick@adenbuie.com>
2025-05-14 17:03:43 -0400

    pre-talk

--- 03-b_shinychat-app.R
+++ 03-b_shinychat-app.R
@@ -4,23 +4,4 @@
 # => pak::pak("posit-dev/shinychat")
 # Use gpt-4.1-nano
 
-library(shiny)
-library(bslib)
-library(ellmer)
-library(shinychat)
-
-ui <- page_sidebar(
-  title = "Demo shinychat app",
-  chat_ui("chat")
-)
-
-server <- function(input, output, session) {
-  client <- chat_openai(model = "gpt-4.1-nano")
-
-  observeEvent(input$chat_user_input, {
-    stream <- client$stream_async(input$chat_user_input)
-    chat_append("chat", stream)
-  })
-}
-
-shinyApp(ui, server)
+# Copy from 03-a
